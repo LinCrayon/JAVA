05cb29c8adb9e704b223c4466f2b720c1892f00c
ther-crayon <13960749008W@gmail.com>
2021-05-24 13:17:57 +0800

    2021/5/24/13:17

---
+++ src/com/linshengqian/base/DataTypeCaseDemon02.java
@@ -0,0 +1,32 @@
+package com.linshengqian.base;
+
+public class DataTypeCaseDemon02 {
+    public static void main(String[] args) {
+        String s= "Hello";
+        //八大基本数据类型
+
+        //整数
+        int num1 = 110;
+        byte num2 = 127;
+        short num3 = 30;
+        long num4 = 30L;//Long类型要在数字后面加L
+
+        //浮点数
+        float num5 = 50.1F;
+        double num6 = 3.14159565358979323846264;
+
+        //字符
+        char name = '林';
+        //字符串
+        //String  name = "crayon";
+        //字符串，String不是关键字,是类
+
+        //布尔值
+        boolean flag = true;
+        //boolean flag = false;
+
+        System.out.println(s);
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
